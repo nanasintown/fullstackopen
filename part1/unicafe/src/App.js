1c1541d2c6547fe08431ee7d81ae41362bb2d2fd
nanacao <nhutmcao@gmail.com>
2023-08-20 14:05:12 +0300

    Upload unicafe

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const onNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const onBadClick = () => {
+    setBad(bad + 1);
+  };
+  return (
+    <div>
+      <div>
+        <h2>Give feedback</h2>
+        <Button text="Good" onClick={onGoodClick} />
+        <Button text="Neutral" onClick={onNeutralClick} />
+        <Button text="Bad" onClick={onBadClick} />
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  if (good || neutral || bad) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <table>
+          <tbody>
+            <StatisticLine text="Good" value={good} />
+            <StatisticLine text="Neutral" value={neutral} />
+            <StatisticLine text="Bad" value={bad} />
+            <StatisticLine text="All" value={all} />
+            <StatisticLine text="Average" value={all / 3} />
+            <StatisticLine text="Positive" value={`${(good / all) * 100}%`} />
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>No feedback given</p>
+    </div>
+  );
+};
+
+const Button = ({ text, onClick }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+export default App;
