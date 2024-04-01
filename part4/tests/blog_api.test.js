e6932ae0a1b3271c0025d34452ac4c09994be5dd
nanacao <nhutmcao@gmail.com>
2024-04-01 17:11:12 +0300

    Exercise 4.9-10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -21,7 +21,49 @@ describe('GET /api/blogs', () => {
   });
   test('returns the correct number of blogs', async () => {
     const response = await api.get('/api/blogs');
-    assert.strictEqual(response.body.length, 6);
+    assert.strictEqual(response.body.length, helper.initialBlogs.length);
+  });
+  test('returns blogs with an id field', async () => {
+    const response = await api.get('/api/blogs');
+    const blogs = response.body;
+
+    blogs.forEach((blog) => {
+      assert.notStrictEqual(blog.id, undefined);
+    });
+  });
+});
+
+describe('POST /api/blogs', () => {
+  const newBlog = {
+    title: 'Test post blog',
+    author: 'Author',
+    url: 'aalto.fi',
+    likes: 0,
+  };
+  test('increases the number of returned blogs by one', async () => {
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const response = await api.get('/api/blogs');
+    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1);
+  });
+  test('adds the proper content to the database', async () => {
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const response = await api.get('/api/blogs');
+
+    const blogTitles = response.body.map((r) => r.title);
+    assert(blogTitles.includes('Test post blog'));
+
+    const blogAuthor = response.body.map((r) => r.author);
+    assert(blogAuthor.includes('Author'));
   });
 });
 
