62bbb53d77e74a2c6cb103b39f102a47f677040d
nanacao <nhutmcao@gmail.com>
2024-04-01 15:58:22 +0300

    Exercise 4.8

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,22 +2,28 @@ const Blog = require('../models/blogs');
 
 const blogRouter = require('express').Router();
 
-blogRouter.get('/', (req, res, next) => {
-  Blog.find({})
-    .then((blogs) => {
-      res.json(blogs);
-    })
-    .catch((err) => next(err));
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogRouter.post('/', (req, res, next) => {
-  const blog = new Blog(req.body);
-  blog
-    .save()
-    .then((result) => {
-      res.status(201).json(result);
-    })
-    .catch((err) => next(err));
+blogRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body);
+
+  const result = await blog.save();
+
+  response.status(201).json(result);
+  // const blog = new Blog(req.body);
+  // blog
+  //   .save()
+  //   .then((result) => {
+  //     res.status(201).json(result);
+  //   })
+  //   .catch((err) => next(err));
+});
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
 });
 
 module.exports = blogRouter;
