afe2ade40dac702c3371fc8014868a9248216351
nanacao <nhutmcao@gmail.com>
2024-04-17 15:05:29 +0300

    4.19

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,7 +1,16 @@
 const Blog = require('../models/blogs');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 const blogRouter = require('express').Router();
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  }
+  return null;
+};
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
@@ -10,7 +19,12 @@ blogRouter.get('/', async (request, response) => {
 
 blogRouter.post('/', async (request, response) => {
   const body = request.body;
-  const user = await User.findById(body.user);
+  // const user = await User.findById(body.user);
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     url: body.url,
