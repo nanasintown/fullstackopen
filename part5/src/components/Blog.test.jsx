e08d6b0aa18c4ff7d21fc119409ffe1ae330b55a
nanacao <nhutmcao@gmail.com>
2024-04-28 13:29:59 +0300

    5.15

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -3,15 +3,16 @@ import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
 describe('Render blogs', () => {
+  const mockUser = { id: '123344569704' };
   const blog = {
     title: 'TestBlog',
     author: 'Sir/Maam',
     url: 'hehehe',
+    user: mockUser,
   };
 
   const mockLike = vi.fn();
   const mockDelete = vi.fn();
-  const mockUser = {};
 
   test('Render only title and author.', () => {
     const { container } = render(
@@ -58,5 +59,36 @@ describe('Render blogs', () => {
     const button = screen.getByText('view');
     await user.click(button);
     screen.getByText('hide');
+  });
+});
+
+describe('Calls event handlers:', () => {
+  const mockUser = { id: '123344569704' };
+
+  const blog = {
+    title: 'TestBlog',
+    author: 'Mr/Ms',
+    url: 'hihihi',
+    user: mockUser,
+  };
+  const mockLike = vi.fn();
+  const mockDelete = vi.fn();
+
+  test('Check if the like button is clicked twice, the event handler is called twice', async () => {
+    render(
+      <Blog
+        blog={blog}
+        likeBlog={mockLike}
+        deleteBlog={mockDelete}
+        user={mockUser}
+      />
+    );
+    const user = userEvent.setup();
+    const viewButton = screen.getByText('view');
+    await user.click(viewButton);
+    const likeButton = screen.getByText('like');
+    await user.click(likeButton);
+    await user.click(likeButton);
+    expect(mockLike.mock.calls).toHaveLength(2);
   });
 });
