75290cce4f20ed575b07c4234705c745efe64a15
nanacao <nhutmcao@gmail.com>
2024-04-28 13:26:43 +0300

    5.14

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
-test('Renders the blogs title and author.', () => {
+describe('Render blogs', () => {
   const blog = {
     title: 'TestBlog',
     author: 'Sir/Maam',
@@ -12,20 +13,50 @@ test('Renders the blogs title and author.', () => {
   const mockDelete = vi.fn();
   const mockUser = {};
 
-  const { container } = render(
-    <Blog
-      blog={blog}
-      likeBlog={mockLike}
-      deleteBlog={mockDelete}
-      user={mockUser}
-    />
-  );
+  test('Render only title and author.', () => {
+    const { container } = render(
+      <Blog
+        blog={blog}
+        likeBlog={mockLike}
+        deleteBlog={mockDelete}
+        user={mockUser}
+      />
+    );
 
-  const hideDetails = container.querySelector('#hide-details');
-  screen.debug(hideDetails);
+    const hideDetails = container.querySelector('#hide-details');
+    expect(hideDetails.textContent).toContain(blog.title);
+    expect(hideDetails.textContent).toContain(blog.author);
+    expect(hideDetails.textContent).not.toContain(blog.url);
+    expect(hideDetails.textContent).not.toContain('Likes:');
+  });
+  test('Renders url, likes and user when click view', () => {
+    const { container } = render(
+      <Blog
+        blog={blog}
+        likeBlog={mockLike}
+        deleteBlog={mockDelete}
+        user={mockUser}
+      />
+    );
 
-  expect(hideDetails.textContent).toContain(blog.title);
-  expect(hideDetails.textContent).toContain(blog.author);
-  expect(hideDetails.textContent).not.toContain(blog.url);
-  expect(hideDetails.textContent).not.toContain('Likes:');
+    const showDetails = container.querySelector('#show-details');
+    expect(showDetails.textContent).toContain(blog.title);
+    expect(showDetails.textContent).toContain(blog.author);
+    expect(showDetails.textContent).toContain(blog.url);
+    expect(showDetails.textContent).toContain('Likes:');
+  });
+  test('Check "hide" button when click view.', async () => {
+    render(
+      <Blog
+        blog={blog}
+        likeBlog={mockLike}
+        deleteBlog={mockDelete}
+        user={mockUser}
+      />
+    );
+    const user = userEvent.setup();
+    const button = screen.getByText('view');
+    await user.click(button);
+    screen.getByText('hide');
+  });
 });
